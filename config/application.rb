f676a15a5e049e9e8bc16ae24decf47b2da3d84d
Thomas <thomaskambale1@gmail.com>
2023-07-04 22:10:29 +0300

    controller updates

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,9 @@ module MotoreviewBackend
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.session_store :cookie_store, expire_after: 86400
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,34 @@
 Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
   get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
+  get '/users/me', to: 'users#show'
+  
 
-
-
-  resources :bikes do
+  resources :bikes, only: [:index, :show, :create, :update, :destroy] do
     resources :reviews
+    
+
   end
 
   resources :reviews do
     put '/like', to: 'reviews#like'
   end
 end
+
+# Rails.application.routes.draw do
+#   post '/signup', to: 'users#create'
+#   post '/login', to: 'sessions#create'
+#   get '/users', to: 'users#index'
+#   get '/users/:id', to: 'users#show'
+
+#   resources :bikes, only: [:index, :show, :create, :update, :destroy] do
+#     resources :reviews
+#   end
+
+#   resources :reviews do
+#     put '/like', to: 'reviews#like'
+#   end
+# end
+
